ad3643c8453a44f3091899ed9b6c24efb88f3089
Sanha Hwang <57973170+hsh6449@users.noreply.github.com>
2023-07-18 22:25:27 +0900

    Update traceAPI.js

--- backend/routes/traceAPI.js
+++ backend/routes/traceAPI.js
@@ -2,6 +2,7 @@ var express = require("express");
 var router = express.Router();
 const sqlite3 = require("sqlite3").verbose();
 const db = new sqlite3.Database("./db/O2ARC.db");
+const fs = require("fs");
 
 /* After deploying , API for traces*/
 router.get("/submission", function (req, res, next) {
@@ -11,18 +12,24 @@ router.get("/submission", function (req, res, next) {
 			console.error(err.message);
 			return res.status(500).send("Error retrieving data");
 		} else {
+			// Log the current working directory
+			console.log(process.cwd());
+
+			// Write data to a JSON file
 			fs.writeFile(
 				"output.json",
 				JSON.stringify(rows, null, 2),
 				"utf8",
 				function (err) {
 					if (err) {
-						console.error(err.message);
-						return res.status(500).send("Error writing file");
+						console.error(err);
+						return;
 					}
-					return res.send(rows);
+					console.log("File has been created");
 				}
 			);
+
+			return res.send(rows);
 		}
 	});
 });
